402925755946724ad0e64b9cfdaec8e2e4a7b397
Selviaibrahim <selviaibrahimgrace@gmail.com>
2024-06-17 08:49:50 +0300

    testing

--- Web-Project-Layout/routes/SJAKroutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const SJAKController = require("../controllers/SJAKController");
-const router = express.Router();
-
-// Route for SJAK page
-router.get("/", SJAKController.showSJAKPage);
-
-module.exports = router;
