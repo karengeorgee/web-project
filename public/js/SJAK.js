0c7707376b2772923d31c55ec86874c49c3930b0
jumanadiaa <168435584+jumanadiaa@users.noreply.github.com>
2024-06-23 00:01:12 +0200

    updated public

--- public/js/SJAK.js
+++ public/js/SJAK.js
@@ -1,65 +1,61 @@
-function toggleMenu() {
-    var menuItems = document.getElementById("menuItems");
-    menuItems.classList.toggle("show");
-}
-
 // Close the menu list when Contact Us or About Us is clicked
-document.querySelectorAll(".menu-items a").forEach(function(link) {
-    link.addEventListener("click", function() {
-        var menuItems = document.getElementById("menuItems");
-        if (menuItems.classList.contains("show")) {
-            toggleMenu();
-        }
-    });
+document.querySelectorAll(".menu-items a").forEach(function (link) {
+  link.addEventListener("click", function () {
+    var menuItems = document.getElementById("menuItems");
+    if (menuItems.classList.contains("show")) {
+      toggleMenu();
+    }
+  });
 });
 
 var calendar = flatpickr("#calendar", {
-    enableTime: true, 
-    dateFormat: "Y-m-d H:i", 
-    minDate: "today", 
-    maxDate: new Date().fp_incr(30), 
+  enableTime: true,
+  dateFormat: "Y-m-d H:i",
+  minDate: "today",
+  maxDate: new Date().fp_incr(30),
 });
 
 // Show the calendar when the "BOOK YOUR TABLE" button is clicked
-document.querySelector(".book-button").addEventListener("click", function() {
-    calendar.open();
+document.querySelector(".book-button").addEventListener("click", function () {
+  calendar.open();
 });
 
-var doneButton = document.createElement('button');
-doneButton.innerHTML = 'Done';
-doneButton.id = 'done-button';
+var doneButton = document.createElement("button");
+doneButton.innerHTML = "Done";
+doneButton.id = "done-button";
 
-doneButton.addEventListener("click", function() {
-    calendar.close();
-    var selectedDateTime = calendar.input.value; // Get the selected date and time
-    var [date, time] = selectedDateTime.split(' ');
-    window.location.href = `/Reservation?date=${date}&time=${time}`;
+doneButton.addEventListener("click", function () {
+  calendar.close();
+  var selectedDateTime = calendar.input.value; // Get the selected date and time
+  var [date, time] = selectedDateTime.split(" ");
+  window.location.href = `/reservation?date=${date}&time=${time}`;
 });
 
 calendar.calendarContainer.appendChild(doneButton);
 
-document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll(".images-container img");
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll(".images-container img");
 
-    function fadeInImages() {
-        images.forEach((image, index) => {
-            if (isElementInViewport(image)) {
-                setTimeout(() => {
-                    image.style.opacity = 1;
-                }, index * 300); 
-            }
-        });
-    }
+  function fadeInImages() {
+    images.forEach((image, index) => {
+      if (isElementInViewport(image)) {
+        setTimeout(() => {
+          image.style.opacity = 1;
+        }, index * 300);
+      }
+    });
+  }
 
-    function isElementInViewport(el) {
-        var rect = el.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    }
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
 
-    window.addEventListener("scroll", fadeInImages);
+  window.addEventListener("scroll", fadeInImages);
 });
--- public/js/userdash.js
+++ public/js/userdash.js
@@ -1,23 +1,17 @@
-
-
-function toggleMenu() {
-    var menuItems = document.getElementById("menuItems");
-    menuItems.classList.toggle("show");
-}
-
 function currentreservations() {
-    
-    var Viewcurrentreservations = document.getElementById("Viewcurrentreservtaions");
-    var currentTableBody = document.getElementById("currentTableBody");
-    Viewcurrentreservations.style.display = "block";
-    menuItems.classList.remove("show");
+  var Viewcurrentreservations = document.getElementById(
+    "Viewcurrentreservtaions"
+  );
+  var currentTableBody = document.getElementById("currentTableBody");
+  Viewcurrentreservations.style.display = "block";
+  menuItems.classList.remove("show");
 
-    fetch('/reservations') // This path should match the route defined in the backend
-        .then(response => response.json())
-        .then(reservations => {
-            currentTableBody.innerHTML = '';
-            reservations.forEach(reservation => {
-                var reservationRow = `
+  fetch("/reservations") // This path should match the route defined in the backend
+    .then((response) => response.json())
+    .then((reservations) => {
+      currentTableBody.innerHTML = "";
+      reservations.forEach((reservation) => {
+        var reservationRow = `
                     <tr>
                         <td>${reservation.firstName} ${reservation.lastName}</td>
                         <td>${reservation._id}</td>
@@ -29,26 +23,26 @@ function currentreservations() {
                         </td>
                     </tr>
                 `;
-                currentTableBody.innerHTML += reservationRow;
-            });
-        })
-        .catch(err => console.error(err));
+        currentTableBody.innerHTML += reservationRow;
+      });
+    })
+    .catch((err) => console.error(err));
 
-    Viewcurrentreservations.scrollIntoView({ behavior: 'smooth' });
+  Viewcurrentreservations.scrollIntoView({ behavior: "smooth" });
 }
 
 function reservationshistory() {
-    var bookinghis = document.getElementById("bookinghis");
-    var historyTableBody = document.getElementById("bookinghistable");
-    bookinghis.style.display = "block";
-    menuItems.classList.remove("show");
+  var bookinghis = document.getElementById("bookinghis");
+  var historyTableBody = document.getElementById("bookinghistable");
+  bookinghis.style.display = "block";
+  menuItems.classList.remove("show");
 
-    fetch('/reservations') // This path should match the route defined in the backend
-    .then(response => response.json())
-    .then(reservations => {
-        currentTableBody.innerHTML = '';
-        reservations.forEach(reservation => {
-            var reservationRow = `
+  fetch("/reservations") // This path should match the route defined in the backend
+    .then((response) => response.json())
+    .then((reservations) => {
+      currentTableBody.innerHTML = "";
+      reservations.forEach((reservation) => {
+        var reservationRow = `
                 <tr>
                     <td>${reservation.firstName} ${reservation.lastName}</td>
                     <td>${reservation._id}</td>
@@ -60,14 +54,13 @@ function reservationshistory() {
                     </td>
                 </tr>
             `;
-            historyTableBody.innerHTML += reservationRow;
-            });
-        })
-        .catch(err => console.error(err));
+        historyTableBody.innerHTML += reservationRow;
+      });
+    })
+    .catch((err) => console.error(err));
 
-    bookinghis.scrollIntoView({ behavior: 'smooth' });
+  bookinghis.scrollIntoView({ behavior: "smooth" });
 }
-
 
 var slideIndex = 0;
 showSlides();
@@ -76,10 +69,17 @@ function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  slides[slideIndex-1].style.display = "block";  
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 2000);
+}
+
+function toggleMenu() {
+  var menuItems = document.getElementById("menuItems");
+  menuItems.classList.toggle("show");
 }
